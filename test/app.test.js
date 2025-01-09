449875f02e4b77e9fa01f599b136560e983029ca
Hack-it-Rafi <zozorafi2016@gmail.com>
2025-01-09 10:56:06 +0600

    test and deploy2

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const express = require('express');
-const app = require('./app');
+const app = require('..');
 
 describe('Express App', () => {
     // Test the root route
